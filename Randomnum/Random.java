c16b4c50f116ee6910626bde4f152727a978cbde
omar <umarfarooqa16@gmail.com>
2021-01-12 21:49:15 +0530

    tictac

--- Randomnum/Random.java
+++ Randomnum/Random.java
@@ -2,6 +2,7 @@ public class Random{
     public static void main(String[] args) {
         long a = System.currentTimeMillis();
         a = a%1000;
+        System.out.print("The random number is : ");
         System.out.println(a);
     }
 }
